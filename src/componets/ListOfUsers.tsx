3850abf336e6b19b587e8b40d580cc80581d3941
wal90 <walteromartinez90@gmail.com>
2023-11-08 12:30:57 -0300

    list of users function

--- src/componets/ListOfUsers.tsx
+++ src/componets/ListOfUsers.tsx
@@ -6,7 +6,7 @@ import {
     TableHead,
     TableHeaderCell,
     TableBody,
-    BadgeDelta
+    
   } from "@tremor/react";
 
   const users: {
@@ -14,9 +14,7 @@ import {
     name: string;
     email: string;
     github: string
-  }[] = []
-  
-  const salesPeople = [
+  }[] = [
     {
       id:"1",
       name: "Walter Martinez",
@@ -62,34 +60,27 @@ import {
     },
   ];
   
-  export default function Example() {
+  export default function ListOfUSers() {
     return (
       <Card>
         <Table>
           <TableHead>
             <TableRow>
-              <TableHeaderCell>Name</TableHeaderCell>
-              <TableHeaderCell className="text-right">Leads</TableHeaderCell>
-              <TableHeaderCell className="text-right">Sales ($)</TableHeaderCell>
-              <TableHeaderCell className="text-right">Quota ($)</TableHeaderCell>
-              <TableHeaderCell className="text-right">Variance</TableHeaderCell>
-              <TableHeaderCell className="text-right">Region</TableHeaderCell>
-              <TableHeaderCell className="text-right">Status</TableHeaderCell>
+              <TableHeaderCell>Id</TableHeaderCell>
+              <TableHeaderCell>Nombre</TableHeaderCell>
+              <TableHeaderCell>Email</TableHeaderCell>
+              <TableHeaderCell>Acciones</TableHeaderCell>
+              
             </TableRow>
           </TableHead>
           <TableBody>
-            {salesPeople.map((item) => (
+            {users.map((item) => (
               <TableRow key={item.name}>
+                <TableCell>{item.id}</TableCell>
                 <TableCell>{item.name}</TableCell>
-                <TableCell className="text-right">{item.leads}</TableCell>
-                <TableCell className="text-right">{item.sales}</TableCell>
-                <TableCell className="text-right">{item.quota}</TableCell>
-                <TableCell className="text-right">{item.variance}</TableCell>
-                <TableCell className="text-right">{item.region}</TableCell>
-                <TableCell className="text-right">
-                  <BadgeDelta deltaType={item.deltaType} size="xs">
-                    {item.delta}
-                  </BadgeDelta>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>
+                 Acciones
                 </TableCell>
               </TableRow>
             ))}
