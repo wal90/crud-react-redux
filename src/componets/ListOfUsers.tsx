ba6daf317ef27f993c6f46d42b74ef5050585707
wal90 <walteromartinez90@gmail.com>
2023-11-08 12:23:46 -0300

    list of users terminada

--- src/componets/ListOfUsers.tsx
+++ src/componets/ListOfUsers.tsx
@@ -19,46 +19,46 @@ import {
   const salesPeople = [
     {
       id:"1",
-      name: "Peter Doe",
-      email: "1,00",
-      github: "1,200,000",
+      name: "Walter Martinez",
+      email: "walteromartinez90@gmail.com",
+      github: "wal90",
     },
     {
       id: "2",
       name: "Lena Whitehouse",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "lena_whitehouse@gmail.com",
+      github: "lenaWhite",
     },
     {
       id: "3",
       name: "Phil Less",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "philless@gmail.com",
+      github: "pilless07",
     },
     {
       id: "4",
       name: "John Camper",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "johncamper0007@gmail.com",
+      github: "john0007",
     },
     {
       id: "5",
       name: "Max Balmoore",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "maxbalmoore01@gmail.com",
+      github: "maxBalmoore",
     },
     {
       id: "6",
       name: "Peter Moore",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "petermoore@gmail.com",
+      github: "petermoore",
    
     },
     {
       id: "7",
       name: "Joe Sachs",
-      email: "1,000,000",
-      github: "1,200,000",
+      email: "joesachs20@gmail.com",
+      github: "joesachs20",
     },
   ];
   
