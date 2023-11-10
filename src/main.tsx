767b04dd192546e7b9e6286bf40f27d9523de044
wal90 <walteromartinez90@gmail.com>
2023-11-10 12:41:27 -0300

    main/ provider/ store

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { store } from './store/index.ts'
+import { Provider } from 'react-redux'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
