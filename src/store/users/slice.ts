9afccd8fce4c84ee680a1e2615a72eb8b54970e2
wal90 <walteromartinez90@gmail.com>
2023-11-11 12:13:47 -0300

    initial state users

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -10,6 +10,51 @@ export interface UserWhithId extends User {
     id: string;
 }
 
+const initialState: UserWhithId[] = [{
+    
+        id:"1",
+        name: "Walter Martinez",
+        email: "walteromartinez90@gmail.com",
+        github: "wal90",
+      },
+      {
+        id: "2",
+        name: "Lena Whitehouse",
+        email: "lena_whitehouse@gmail.com",
+        github: "lenaWhite",
+      },
+      {
+        id: "3",
+        name: "Phil Less",
+        email: "philless@gmail.com",
+        github: "pilless07",
+      },
+      {
+        id: "4",
+        name: "John Camper",
+        email: "johncamper0007@gmail.com",
+        github: "john0007",
+      },
+      {
+        id: "5",
+        name: "Max Balmoore",
+        email: "maxbalmoore01@gmail.com",
+        github: "maxBalmoore",
+      },
+      {
+        id: "6",
+        name: "Peter Moore",
+        email: "petermoore@gmail.com",
+        github: "petermoore",
+     
+      },
+      {
+        id: "7",
+        name: "Joe Sachs",
+        email: "joesachs20@gmail.com",
+        github: "joesachs20",
+      
+},]
 export const usersSlice = createSlice({
     name: 'users',
     initialState: ,
