15ac83a22b0202794a2b57a73882207ce127c750
wal90 <walteromartinez90@gmail.com>
2023-11-11 12:08:34 -0300

    interface user

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -1,5 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export interface User {
+    name: string;
+    email: string;
+    github: string
+}
 export const usersSlice = createSlice({
     name: 'users',
     initialState: ,
