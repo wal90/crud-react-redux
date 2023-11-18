a623baee8b31f91c6e684490b42cf6bdf5cdb9ef
wal90 <walteromartinez90@gmail.com>
2023-11-18 14:00:41 -0300

    persistance local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice"
 
 // cada funcion se ejecuta en tres momentos diferentes... recibe la store, una forma para ir a la siguiente y luego la accion
-const persistanceMiddleware = (store) => (next) => (action) => {
-
+const persistanceLocalStoregeMiddleware = (store) => (next) => (action) => {
+    next(action);
+    localStorage.setItem("__redux__state__", JSON.stringify(store.getState()))
 }
 
 export const store = configureStore({
     reducer: {
         users: usersReducer
-    }
-    middleware: [persistanceMiddleware]
+    },
+    middleware: [persistanceLocalStoregeMiddleware]
 })
 
 export type RootState = ReturnType<typeof store.getState> // retorna el typo que tiene la funcion
