28d5c2a52dde938bb27891dcbe14a432057f1e89
wal90 <walteromartinez90@gmail.com>
2023-11-21 18:56:20 -0300

    function create

--- src/componets/CreateNewUser.tsx
+++ src/componets/CreateNewUser.tsx
@@ -1,1 +1,9 @@
 import { Button, Card, TextInput, Title } from "@tremor/react";
+
+export function CreateNewUSer () {
+    return (
+        <Card>
+            <Title>Create New User </Title>
+        </Card>
+    )
+}
