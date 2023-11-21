a6abd84b5dfd664f5a9806e8c5d7bbb88c195599
wal90 <walteromartinez90@gmail.com>
2023-11-21 18:59:05 -0300

    form

--- src/componets/CreateNewUser.tsx
+++ src/componets/CreateNewUser.tsx
@@ -2,8 +2,21 @@ import { Button, Card, TextInput, Title } from "@tremor/react";
 
 export function CreateNewUSer () {
     return (
-        <Card>
+        <Card style={{ marginTop: '16ox' }}>
             <Title>Create New User </Title>
+
+            <form className="">
+                <TextInput
+                placeholder='Aquí el nombre'
+                />
+                <TextInput
+                placeholder='Aquí el email'
+                />
+                <TextInput
+                placeholder='Aquí el usuario de GitHub'
+                />
+
+            </form>
         </Card>
     )
 }
